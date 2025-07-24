272a4e1f920083c270aa02757311363ee2044554
Priyanka thakur <priyankathakurdnp@gmail.com>
2025-07-25 03:31:47 +0530

    Fix ESLint issues and add README

--- src/components/OrderBookCanvas.tsx
+++ src/components/OrderBookCanvas.tsx
@@ -8,13 +8,13 @@ import { scaleLinear } from "d3-scale";
 import * as THREE from "three";
 import { useMemo } from "react";
 
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Bar({
   entry,
   isBid,
   index,
   colorScale,
-}: {// eslint-disable-next-line @typescript-eslint/no-explicit-any
+}: {
   entry: any;
   isBid: boolean;
   index: number;
